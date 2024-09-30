eb082b2aaaf06d9a648a190f13336f52c7ae5645
Enzo Monteiro <enzovila.monteiro@gmail.com>
2024-09-29 23:48:53 -0300

    feat: mobile for articles

--- green-safe-front-end/src/pages/landing-page/articles/Articles.tsx
+++ green-safe-front-end/src/pages/landing-page/articles/Articles.tsx
@@ -1,8 +1,10 @@
 export const Articles = () => {
   return (
     <section className="lg:max-w-screen-xl lg:m-auto py-8">
-      <h1 className="text-3xl font-medium">Artigos Populares</h1>
-      <div className="flex py-6 justify-between h-[41rem]">
+      <h1 className="text-3xl lg:text-left text-center font-medium">
+        Artigos Populares
+      </h1>
+      <div className="flex py-6 lg:flex-row lg:justify-between lg:h-[41rem] flex-col lg:space-y-0 space-y-6">
         <div className="h-full relative">
           <img
             alt="Article 1"
@@ -10,11 +12,11 @@ export const Articles = () => {
             className="h-full"
             src="./src/assets/article1.jpg"
           />
-          <div className="bg-black bg-opacity-80 text-white absolute bottom-0 left-0 h-1/6 w-full p-4">
+          <div className="bg-black bg-opacity-80 text-white absolute bottom-0 left-0 lg:h-1/6 h-1/2 w-full p-4">
             <h2 className="font-bold text-lg">
               Biotecnologia na Agricultura: Inovação para um Futuro Sustentável
             </h2>
-            <p className="text-sm">
+            <p className="text-sm truncate">
               A biotecnologia na agricultura revoluciona práticas agrícolas,
               melhorando a produtividade e sustentabilidade. Com técnicas como a
               engenharia genética e biofertilizantes, enfrenta desafios como
@@ -23,7 +25,7 @@ export const Articles = () => {
           </div>
         </div>
         <div className="flex flex-col space-y-4">
-          <div className="h-48 relative">
+          <div className="lg:h-48 h-full relative">
             <img
               alt="Article 2"
               loading="lazy"
@@ -39,7 +41,7 @@ export const Articles = () => {
               </p>
             </div>
           </div>
-          <div className="h-48 relative">
+          <div className="lg:h-48 relative">
             <img
               alt="Article 3"
               className="h-full w-full"
@@ -53,7 +55,7 @@ export const Articles = () => {
               </p>
             </div>
           </div>
-          <div className="h-48 relative">
+          <div className="lg:h-48  relative">
             <img
               alt="Article 4"
               className="h-full w-full"
